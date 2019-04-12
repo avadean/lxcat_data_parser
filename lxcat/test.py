e66503b519527c457927585069064a3ee774ebb5
Alise Chachereau <alisec@ethz.ch>
2019-04-12 15:52:49 +0200

    shortened test.py

--- lxcat/test.py
+++ lxcat/test.py
@@ -1,12 +1,12 @@
-import import_functions as imp
+import import_tools as imp
 
 imp.filename_from_path('pa/t.test')
 
 lxcat_swarm_data = imp.import_lxcat_swarm_data('Z:/projects/Sparx/07_own_publications/02_journals/2019_CO2/data/Other_references/Bhalla_1960_alphaN.txt')
-print(lxcat_swarm_data)
+print(lxcat_swarm_data['Parameter'])
 
 print('############################################')
 
 bolsig_data = imp.import_bolsig_output('test_data/output.dat')
-print(bolsig_data.keys())
-print(bolsig_data['CO2    Excitation    0.34 eV'])
+# print(bolsig_data.keys())
+print(bolsig_data['CO2    Excitation    0.34 eV'][-1])
