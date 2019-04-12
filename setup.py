11d0a45c7326e333ce4aaa210cc3c3653ef59224
Alise Chachereau <alisec@ethz.ch>
2019-04-12 15:46:28 +0200

    Creating package

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="lxcat",
+    version="0.0.1",
+    author="Alise Chachereau",
+    author_email="alisec@ethz.ch",
+    description="A set of tools for importing swarm data downloaded from LXcat.",
+    packages=['lxcat'],
+)
