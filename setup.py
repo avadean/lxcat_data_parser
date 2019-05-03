72c8e0a4fa6ccc61f747ee1b4386afdf46cabe94
Alise Chachereau <alisec@ethz.ch>
2019-05-03 11:07:51 +0200

    added classifiers and dependencies

--- setup.py
+++ setup.py
@@ -5,6 +5,14 @@ setuptools.setup(
     version="0.0.1",
     author="Alise Chachereau",
     author_email="alisec@ethz.ch",
-    description="A set of tools for importing swarm data downloaded from LXcat.",
+    description="A set of tools for importing data from the LXcat project.",
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Scientific/Engineering :: Physics'
+    ],
     packages=['lxcat'],
+    install_requires=[
+          'numpy',
+          'enum',
+    ]
 )
